723e26d8e473ba9a4bb40c6ca90a6f18d6cca0b3
amjedBllr <bamjed86@gmail.com>
2024-05-18 00:06:12 +0100

    update

--- app.js
+++ app.js
@@ -36,12 +36,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-app.use(cors({
-    origin: '*',
-    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD','PATCH','DELETE'],
-    credentials: true
-  }));
+app.use(cors())
 
 app.use(session)
 
